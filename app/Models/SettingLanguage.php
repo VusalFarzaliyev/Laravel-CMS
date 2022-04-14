99e0daf4e3d411a371eaa28cca92919e5af3969e
Vusal <vusalferzeliyev5@gmail.com>
2022-04-14 11:04:28 +0400

    CMS

---
+++ app/Models/SettingLanguage.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SettingLanguage extends Model
+{
+    protected $table = 'setting_languages';
+    protected $fillable = [
+        'name','code'
+    ];
+    use HasFactory;
+}
